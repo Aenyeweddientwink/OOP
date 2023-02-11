ac8476090eef4db79fe1cf2fe699dfb7bcb9ed81
Aenyeweddientwink <Aenyeweddientwink@users.noreply.github.com>
2023-02-11 02:31:39 +0000

    Deploying to gh-pages from @ Aenyeweddientwink/OOP@bd92789f30909b7c8130073af04dfd06c4bd26a9 🚀

---
+++ Task-1.2.1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"io.github.aenyeweddientwink","c":"Stack","l":"count()"},{"p":"io.github.aenyeweddientwink","c":"Stack","l":"pop()"},{"p":"io.github.aenyeweddientwink","c":"Stack","l":"push(T)"},{"p":"io.github.aenyeweddientwink","c":"Stack","l":"Stack()","url":"%3Cinit%3E()"}]
