b032dfad5061fee50f20e072044639cff46284d0
Aenyeweddientwink <aenyeweddientwink@gmail.com>
2023-02-18 01:04:29 +0700

    Commit 2
    Commit with tests and proper implementation

--- Task-1.2.1/src/main/java/io/github/aenyeweddientwink/Stack.java
+++ Task-1.2.1/src/main/java/io/github/aenyeweddientwink/Stack.java
@@ -3,21 +3,31 @@ package io.github.aenyeweddientwink;
 import java.util.EmptyStackException;
 
 /**
- * Implementation of a Generic class Stack
- * @param <T> type of elements of the Stack
+ * Implementation of a Generic class Stack.
+ * @param <T> type of elements of the Stack.
  */
 public class Stack<T> {
     private int capacity;
     private int size;
     private T[] stack;
 
+    /**
+     * Creates of an object of Stack class.
+     */
     public Stack(){
         this.capacity = 4;
         this.size = 0;
         this.stack = (T[]) new Object[capacity];
     }
 
+    /**
+     * Pushes an element on the top of the Stack
+     * @param elem element to push
+     */
     public void push(T elem){
+        if (elem == null){
+            throw new NullPointerException("Can't push a null");
+        }
         if (size == capacity){
             this.capacity *= 2;
             T[] newStack = (T[]) new Object[capacity];
@@ -26,6 +36,11 @@ public class Stack<T> {
         }
         this.stack[size++] = elem;
     }
+
+    /**
+     * Pops an element off the top of the Stack
+     * @return the element that was popped
+     */
     public T pop(){
         if (size == 0){
             throw new EmptyStackException();
@@ -38,6 +53,43 @@ public class Stack<T> {
         }
         this.size--;
         return this.stack[size];
+    }
+
+    /**
+     * Pops num elements off the top of the stack and returns them in the form of a stack
+     * @param num number of elements you want to be popped
+     * @return New stack with num elements which were popped
+     */
+    public Stack<T> popStack(int num){
+        if (size < num){
+            throw new EmptyStackException();
+        }
+        Stack<T> newStack = new Stack<>();
+        for (int i =(size-num); i < size; i++){
+            newStack.push(stack[i]);
+        }
+        size = size - num;
+        if (size <= capacity/2){
+            this.capacity = size;
+            T[] lessStack = (T[]) new Object[capacity];
+            System.arraycopy(stack, 0, lessStack, 0, size);
+            this.stack = lessStack;
+        }
+        return newStack;
+    }
+
+    /**
+     * Pushes all elements from otherStack to this Stack
+     * @param otherStack Stack from which elements will be taken to be pushed
+     */
+    public void pushStack(Stack<T> otherStack){
+        if (otherStack == null){
+            throw new NullPointerException("Can't push an empty stack");
+        }
+        int count = otherStack.count();
+        for (int i = 0; i < count;i++){
+            push(otherStack.stack[i]);
+        }
     }
     public int count(){
         return this.size;
--- Task-1.2.1/src/test/java/io/github/aenyeweddientwink/StackTest.java
+++ Task-1.2.1/src/test/java/io/github/aenyeweddientwink/StackTest.java
@@ -1,11 +1,40 @@
 package io.github.aenyeweddientwink;
 
 import org.junit.jupiter.api.Test;
+import java.util.EmptyStackException;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 public class StackTest {
+
+    @Test
+    void stackcreationTest(){
+        Stack<String> stack = new Stack<>();
+        assertEquals(0,stack.count());
+    }
+
+    @Test
+    void popfromemptystackTest(){
+        Stack<String> stack = new Stack<>();
+        assertThrows(EmptyStackException.class, () -> stack.pop());
+    }
+
+    @Test
+    void pushnullpointerTest(){
+        Stack<String> stack = new Stack<>();
+        assertThrows(NullPointerException.class, ()->stack.push(null));
+        assertThrows(NullPointerException.class, ()-> stack.pushStack(null));
+    }
+
+    @Test
+    void capacityTest(){
+        Stack <Integer> stack = new Stack<>();
+        for (int i = 0; i < 8; i++){
+            stack.push(1);
+        }
+        assertEquals(8,stack.count());
+    }
     @Test
     void simpleTest(){
         Stack<Integer> stack = new Stack<>();
@@ -14,9 +43,43 @@ public class StackTest {
         stack.push(2);
         stack.push(3);
 
-        assertEquals(stack.count(),3);
+        assertEquals(3,stack.count());
 
-        assertEquals(stack.pop(),3);
-        assertEquals(stack.count(),2);
+        assertEquals(3,stack.pop());
+        assertEquals(2,stack.count());
     }
+    @Test
+    void pushStackTest(){
+        Stack<String> newstack = new Stack<>();
+        Stack<String> oldstack = new Stack<>();
+        oldstack.push("Hello");
+        oldstack.push("World");
+        String[] array = {"1","2","3","4"};
+        for(int i = 0; i < 4; i++)
+        {
+            oldstack.push(array[i]);
+        }
+        newstack.pushStack(oldstack);
+        assertEquals(6,newstack.count());
+        assertEquals("4",newstack.pop());
+        assertEquals("3",newstack.pop());
+        assertEquals(4,newstack.count());
+    }
+
+    @Test
+    void popStackTest(){
+        Stack<Integer> stack =  new Stack<>();
+        for (int i = 0; i <5; i++){
+            stack.push(i);
+        }
+        assertEquals(5,stack.count());
+        Stack<Integer> stack1 = stack.popStack(3);
+        assertEquals(3,stack1.count());
+        assertEquals(1,stack.pop());
+    }
+
+
+
+
+
 }
