d21064ba6e0da92466101e0ae32c2ca787dae23b
Aenyeweddientwink <aenyeweddientwink@gmail.com>
2023-03-12 19:10:44 +0700

    Commit1

--- Task-1.4.1/src/test/java/io/github/aenyeweddientwink/RecordTest.java
+++ Task-1.4.1/src/test/java/io/github/aenyeweddientwink/RecordTest.java
@@ -34,4 +34,25 @@ public class RecordTest {
         assertEquals(true,Book1.isAbletofinishwithHonors());
     }
 
+    @Test
+    public void MyTest(){
+        RecordBook Mybook = new RecordBook("Artyom","Solovev",1);
+        Mybook.addRecord("ImperativeProgramming","Nesterenko",3,1);
+        Mybook.addRecord("PE","Mamekov",5,1);
+        Mybook.addRecord("DeclarativeProgramming","Vlasov",5,1);
+        Mybook.addRecord("Calculus","Rudometova",4,1);
+
+        Mybook.changeSemester(2);
+
+        Mybook.addRecord("ImperativeProgramming","Nesterenko",3,2);
+        Mybook.addRecord("PE","Mamekov",5,2);
+        Mybook.addRecord("DeclarativeProgramming","Vlasov",4,2);
+        Mybook.addRecord("Calculus","Rudometova",4,2);
+
+        assertEquals(2,Mybook.getSemester());
+        assertEquals(false,Mybook.isAbletogetIncreasedStipend());
+        assertEquals(false,Mybook.isAbletofinishwithHonors());
+        assertEquals((float) 17/4, Mybook.SemesterAverageMark(1));
+        assertEquals((float) 33/8,Mybook.AlltimeaverageMark());
+    }
 }
