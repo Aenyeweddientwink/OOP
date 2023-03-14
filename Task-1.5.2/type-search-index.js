c377e01fd3a83ed51a5a7465494414bd6c8197ce
Aenyeweddientwink <Aenyeweddientwink@users.noreply.github.com>
2023-03-14 13:51:26 +0000

    Deploying to gh-pages from @ Aenyeweddientwink/OOP@cb122ac5b58cd532793257401675caf166e89412 🚀

---
+++ Task-1.5.2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.aenyeweddientwink","l":"Diary"},{"p":"io.github.aenyeweddientwink","l":"Main"},{"p":"io.github.aenyeweddientwink","l":"Note"}]
